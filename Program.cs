e4a3398c963fa1855839d0801944389b84200768
Dylan Smith <dylan-smith@github.com>
2021-04-21 10:13:33 -0500

    created new console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CircleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
